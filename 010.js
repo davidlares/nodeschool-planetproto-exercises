da980842dfcac0ea0a7f4774037bb9881265061b
David Lares <david.e.lares@gmail.com>
2018-11-06 22:46:07 -0400

    Exercises

---
+++ 010.js
@@ -0,0 +1,14 @@
+// -> Create an object called 'robot' using an object literal
+// -> robot should have a property 'smart' with value true
+
+var robot = {
+	'smart': true
+};
+// -> Claim the result robot.smart
+claim(robot.smart, robot.smart);
+
+// ------------------------------------------------
+// Common JS exports for verification, don't modify
+module.exports = {
+	robot: robot
+}
